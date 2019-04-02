37328bf1dba0ecf2a374ee98a16ed8bce71edec3
marb61a <marb61a@gmail.com>
2019-04-02 12:04:46 +0100

    updated navbar

--- src/components/Globals/Navbar.js
+++ src/components/Globals/Navbar.js
@@ -5,13 +5,40 @@ export default class Navbar extends Component {
   state = {  
     navbarOpen: false,
     css: "collapse navbar-collapse ",
-    
+    links: [
+      {
+        id: 1,
+        path: "/",
+        text: "home"
+      },
+      {
+        id: 2,
+        path: "/about",
+        text: "about"
+      }
+    ]
   };
+
+  navbarHandler = () => {
+
+  }
 
   render() {
     return (
-      <nav>
-      
+      <nav className="navbar navbar-expand-sm bg-light navbar-light">
+        <Link to="/" className="navbar-brand">
+        
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={this.navbarHandler}
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="">
+        
+        </div>
       </nav>
     );
   }
