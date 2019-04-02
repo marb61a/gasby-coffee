db4a0b1746ad9cf116a60206d7e740e7859e7d52
marb61a <marb61a@gmail.com>
2019-04-02 11:57:00 +0100

    Added navbar and title Global files

---
+++ src/components/Globals/Navbar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Link } from "gatsby";
+
+export default class Navbar extends Component {
+  state = {  
+    navbarOpen: false,
+    css: "collapse navbar-collapse ",
+    
+  };
+
+  render() {
+    return (
+      <nav>
+      
+      </nav>
+    );
+  }
+}
