d20862077d6674fb899d126b7fd416fd45083880
marb61a <marb61a@gmail.com>
2019-04-04 20:20:43 +0100

    Updated Menu component

--- src/components/Home/Menu.js
+++ src/components/Home/Menu.js
@@ -2,24 +2,47 @@ import React, { Component } from "react";
 import Img from "gatsby-image";
 
 const getCategories = items => {
+  let tempItems = items.map(items => {
+    return items.node.category;
+  });
 
+  let tempCategories = new Set(tempItems);
+  let categories = Array.from(tempCategories);
+  categories = ["all", ...categories];
+  return categories;
 };
 
 export default class Menu extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      categories: getCategories
+      items: props.items.edges,
+      coffeeItems: props.items.edges,
+      categories: getCategories(props.items.edges)
+    };
+  }
+
+  handleItems = category => {
+    let tempItems = [...this.state.items];
+
+    if(category === 'all') {
+      this.setState(() => {
+        return {coffeeItems: tempItems};
+      });
+    } else {
+
     }
   }
 
   render() {
-    return (
-      <section>
-        <div className="container">
-        
-        </div>
-      </section>
-    );
+    if (this.state.items.length > 0) {
+      return (
+        <section className="menu py-5">
+          <div className="container">
+          
+          </div>
+        </section>
+      );
+    }
   }
 }
