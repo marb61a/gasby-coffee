a845d238a105f3be7084cca8d87e0e2906ba8415
marb61a <marb61a@gmail.com>
2019-04-11 12:09:46 +0100

    Updated navbar render method

--- src/components/Globals/Navbar.js
+++ src/components/Globals/Navbar.js
@@ -31,13 +31,13 @@ export default class Navbar extends Component {
       navbarOpen: true,
       css: "collapse navbar-collapse show"
     });
-  }
+  };
 
   render() {
     return (
       <nav className="navbar navbar-expand-sm bg-light navbar-light">
         <Link to="/" className="navbar-brand">
-        
+          <img src={logo} alt="logo" />
         </Link>
         <button
           className="navbar-toggler"
@@ -46,8 +46,26 @@ export default class Navbar extends Component {
         >
           <span className="navbar-toggler-icon" />
         </button>
-        <div className="">
-        
+        <div className={this.state.css}>
+          <ul className="navbar-nav mx-auto">
+            {
+              this.state.links.map(link => {
+                return(
+                  <li key={link.id} className="nav-item">
+                    <Link 
+                      to={link.path}
+                      className="nav-link text-capitalize"
+                    >
+                      {link.text}
+                    </Link>
+                  </li>
+                );
+              })
+            }
+            <li className="nav-item ml-sm-5">
+              <FaCartArrowDown className="cart-icon snipcart-checkout" />
+            </li>
+          </ul>
         </div>
       </nav>
     );
